d6d6a37f8b114e46702751afc375eea50f4cd4d9
Tadeo Grach <tade.g.17@gmail.com>
2020-11-25 17:07:59 +0000

    fixing main

--- main.c
+++ main.c
@@ -55,9 +55,9 @@ int main(int ac, char **av, char **env)
  */
 int main_extension(char **args, char *path, char *buffer, char **env)
 {
-	/*struct stat st;*/
+	struct stat st;
 
-	if (/*stat(args[0], &st) != 0*/args[0][0] != '/' && args[0][0] != '.')
+	if (stat(args[0], &st) != 0/*args[0][0] != '/' && args[0][0] != '.'*/)
 	{
 		path = getpath_4(args[0], env);
 		if (path == NULL)
